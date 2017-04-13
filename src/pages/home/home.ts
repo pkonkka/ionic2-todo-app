3374f65a7c88447f8b9e6b857696266f21a6eb50
Petri Konkka <petrikonkka@gmail.com>
2017-04-13 14:10:30 +0300

    Added loading controller for the home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from 'ionic-angular';
 
-// import { Observable } from 'rxjs/Rx';
 import { Todo } from '../../models/todo';
 
 import { TodoService } from '../../services/todo';
@@ -14,15 +14,21 @@ export class HomePage implements OnInit {
   allTodos: Todo[] = [];
   filteredTodos: Todo[] = [];
 
-  constructor(private todoService: TodoService) {}
+  constructor(private todoService: TodoService, private loadingCtrl: LoadingController) {}
 
   // ------------------------------------------------------------------
   ngOnInit() {
+
+    const loader = this.loadingCtrl.create({
+      content: 'Wait a moment...'
+    });
+    loader.present();
     this.todoService.getAllTodos()
       .subscribe(
         todos => { 
           this.allTodos = this.filteredTodos = todos;
           this.filterOutCompletedTasks();
+          loader.dismiss();
         });
   }
 
